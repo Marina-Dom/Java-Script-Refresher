3e4fe66843314745bc3da555b00c2ee4210260f0
marina <marina-dominguez@alumni.nmu.edu>
2022-07-07 17:32:09 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+document.write("JavaScript works!");
+
+let firstName = "Marina";
+let lastName = "Dominguez";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+let population = 672351;
+let population3 = population * 3;
+
+console.log(population);
+console.log(population3);
+
+const javaScriptIsCool = true;
+
+// if (javaScriptIsCool) {
+// }
+
+console.log(javaScriptIsCool);
+
+const color = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+console.log(color);
+
+for (let i = 0; i < 6; i++) {
+    console.log(i);
+}
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+// Backwards loop
+// for (let i = 5; i != -1; i--) {
+//     console.log(i);
+// }
